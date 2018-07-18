88725a111ef019c2779eaa689b125c1e3ab148ef
harikrishna8554 <hanishkrishna27@gmail.com>
2018-07-18 09:44:14 +0530

    new git demo data entry

---
+++ src/test/java/DemoRunner1.java
@@ -0,0 +1,34 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class DemoRunner1 {
+	WebDriver driver;
+	@BeforeMethod
+	public void setUp()
+	{
+		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(160, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(160, TimeUnit.SECONDS);
+		driver.manage().deleteAllCookies();
+	}
+	@Test
+	public void test1() throws Exception
+	{
+		driver.get("https://www.tcs.com/");
+		String title = driver.getTitle();
+		System.out.println(title);
+		Thread.sleep(5000);
+	}
+	@AfterMethod
+	public void tearDown()
+	{
+		driver.quit();
+	}
+
+}
